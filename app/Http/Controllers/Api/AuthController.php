267c9f62978c3abda195b88bef5455e142d7c696
Noah <practicasjaguar@gmail.com>
2024-05-06 00:01:34 -0500

    Configurando sanctum

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+final class AuthController extends Controller
+{
+    final public function login(): JsonResponse
+    {
+        request()->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required'
+        ]);
+
+        $user = User::where('email', request()->email)->first();
+
+        if (! $user || ! Hash::check(request()->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return response()->json([
+            'token' => $user->createToken(request()->device_name)->plainTextToken
+        ]);
+    }
+
+    final public function logout(): JsonResponse
+    {
+        auth()->user()->tokens()->delete();
+
+        return response()->json([
+            'message' => 'Logged out'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+/** PERSONAL API TOKENS WITH SANCTUM! */
 Route::get('/test', function(){
     return [
         "name" => "Noah",
@@ -11,6 +13,13 @@ Route::get('/test', function(){
     ];
 });
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function ()
+{
+    Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        //return $request->user();
+        return Auth::user();
+    });
+});
